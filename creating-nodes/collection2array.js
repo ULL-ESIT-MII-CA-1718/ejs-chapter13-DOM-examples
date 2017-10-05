5ca1614e7c570dc8d9c71bdae986e620a5e54fc0
Casiano Rodriguez Leon <casiano.rodriguez.leon@gmail.com>
2017-10-05 11:27:10 +0100

    creating-nodes/collection2array.html

--- creating-nodes/collection2array.js
+++
@@ -1,9 +0,0 @@
-var arrayish = {0: "one", 1: "two", length: 2};
-var real = Array.prototype.slice.call(arrayish, 0);
-real.forEach(function(elt) { console.log(elt); });
-// → one
-//   two
-var ar = Array.from(arrayish);
-ar.forEach(function(elt) { console.log(elt); });
-// → one
-//   two
